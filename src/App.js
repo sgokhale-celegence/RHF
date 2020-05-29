a430514e6f8e33de16bc5b3a25958f1ee391adcf
sgokhale-celegence <64236155+sgokhale-celegence@users.noreply.github.com>
2020-05-29 20:19:10 +0530

    basic example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import "./styles.css";
 
 export default function App() {
@@ -6,6 +7,33 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <LoginForm />
     </div>
+  );
+}
+
+export function LoginForm() {
+  const { register, handleSubmit, errors, watch } = useForm();
+  console.log(watch("login")); // watch input value by passing the name of it
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="login" ref={register} />
+      <input
+        name="password"
+        type="password"
+        ref={register({
+          required: "Please provide a Password",
+          minLength: { value: 10, message: "too short" }
+        })}
+      />
+      {errors.password && <span>{errors.password.message}</span>}
+
+      <input type="submit" />
+    </form>
   );
 }
